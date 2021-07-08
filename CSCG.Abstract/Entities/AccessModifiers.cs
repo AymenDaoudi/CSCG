44e1307522a4378877d65762d22c03d323b250aa
Aymen Daoudi <aymen.daoudi@oneocean.com>
2021-07-08 01:45:55 -0400

    Try CI by changing CSCG.Abstract only

--- CSCG.Abstract/Entities/AccessModifiers.cs
+++ CSCG.Abstract/Entities/AccessModifiers.cs
@@ -2,6 +2,16 @@
 
 namespace CSCG.Abstract.Entities
 {
+    /// <summary>
+    /// Type that represents possible access modifiers.
+    /// Un object of this type can be used as a flag
+    /// <example>
+    /// This sample shows how to use <see cref="AccessModifiers"/> as a flag.
+    /// <code>
+    /// AccessModifiers.Public | AccessModifiers.Protected
+    /// </code>
+    /// </example>
+    /// </summary>
     [Flags]
     public enum AccessModifiers : sbyte
     {
