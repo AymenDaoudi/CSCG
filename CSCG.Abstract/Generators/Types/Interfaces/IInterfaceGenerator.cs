ff6357588efa8068a326db3c624997dcb73a94cb
Aymen Daoudi <aymen.daoudi@oneocean.com>
2021-07-12 15:59:24 -0400

    Fixed InterfaceGenerator type parameters

--- CSCG.Abstract/Generators/Types/Interfaces/IInterfaceGenerator.cs
+++ CSCG.Abstract/Generators/Types/Interfaces/IInterfaceGenerator.cs
@@ -1,10 +1,13 @@
 ﻿using CSCG.Abstract.Entities;
+using CSCG.Abstract.Entities.Methods.Interfaces;
 using CSCG.Abstract.Entities.Types.Interfaces;
 
 namespace CSCG.Abstract.Generators.Types.Interfaces
 {
-    public interface IInterfaceGenerator<TInterface> where TInterface : InterfaceEntityBase
+    public interface IInterfaceGenerator<TInterface, TMethod> 
+        where TInterface : InterfaceEntityBase
+        where TMethod : InterfaceMethodEntity
     {
-        IInterfaceGenerator<TInterface> Initialize(string interfaceName, AccessModifiers accessModifiers);
+        IInitializedInterfaceGenerator<TInterface, TMethod> Initialize(string interfaceName, AccessModifiers accessModifiers);
     }
 }
